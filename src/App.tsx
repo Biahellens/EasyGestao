59bf0992e057c621db530ee580eb02dbadd36a00
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-02-23 09:02:04 -0300

    :tada: Iniciando Projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRoutes from './routes/routes'
+import GlobalStyle from './theme/theme'
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Content,
+} from './style'
+
+function Home() {
+
+  return (
+    <Content>
+      <p>oi</p>
+    </Content>
+  )
+}
+
+export default Home
