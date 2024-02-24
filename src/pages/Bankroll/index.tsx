43309f70a09e8d30a6cc958e5947306ec61e2c11
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-02-24 16:39:30 -0300

    :sparkles: feat(Bankroll): Adicionando conteudo da page e responsivo

--- src/pages/Bankroll/index.tsx
+++ src/pages/Bankroll/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
-import { Content, Main } from './style'
+import {
+  Content,
+  Main,
+  FormContent,
+  ItemContent,
+  Input,
+  Label,
+  Option,
+  Select,
+  Button,
+  ButtonContent,
+  Title,
+  TitleContent,
+  Img,
+} from './style'
 
 import { NavBar, Header } from '../../components'
+
+import toBack from '../../assets/toBack.svg'
+import attach_money from '../../assets/attach_money.svg'
 
 function Bankroll() {
   return (
@@ -9,7 +26,46 @@ function Bankroll() {
       <NavBar />
       <Content>
         <Header />
-        BACKROLL
+        <TitleContent>
+          <Img src={toBack} $size />
+          <Img src={attach_money} />
+          <Title>Adicionar banca inicial</Title>
+        </TitleContent>
+        <FormContent>
+          <ItemContent>
+            <Label>Nome do Bankroll</Label>
+            <Input placeholder="Escreva aqui" />
+          </ItemContent>
+          <ItemContent>
+            <Label>Tipo de Gestão</Label>
+            <Select name="type-gestao">
+              <Option value="">Buscar item</Option>
+              <Option></Option>
+            </Select>
+          </ItemContent>
+          <ItemContent>
+            <Label>Escolha a Moeda</Label>
+            <Select name="moeda">
+              <Option value="">Buscar item</Option>
+              <Option></Option>
+            </Select>
+          </ItemContent>
+          <ItemContent>
+            <Label>Banca Inicial</Label>
+            <Input placeholder="Escreva aqui" />
+          </ItemContent>
+          <ItemContent>
+            <Label>% Stake</Label>
+            <Input placeholder="Escreva aqui" />
+          </ItemContent>
+          <ItemContent>
+            <Label>Unidade</Label>
+            <Input placeholder="Escreva aqui" />
+          </ItemContent>
+          <ButtonContent>
+            <Button type="submit">Salvar alterações</Button>
+          </ButtonContent>
+        </FormContent>
       </Content>
     </Main>
   )
--- src/pages/Bankroll/style.tsx
+++ src/pages/Bankroll/style.tsx
@@ -9,10 +9,157 @@ export const Main = styled.div`
 export const Content = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
   flex-wrap: wrap;
   justify-content: center;
   background-color: #1c1d33;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+
+export const TitleContent = styled.div`
+  width: 100%;
+  text-align: start;
+  align-items: center;
+  display: flex;
+  margin-top: 2rem;
+  margin-left: 4rem;
+
+  @media (max-width: 768px) {
+    margin-top: 0.5rem;
+    margin-left: 0.5rem;
+  }
+}
+`
+
+export const Title = styled.text`
+  font-size: 24px;
+  line-height: 24px;
+  font-weight: 500;
+  color: #fbfbfb;
+`
+
+export const Img = styled.img<{ $size?: boolean }>`
+  width: ${(props) => (props.$size ? '48px' : '32px')};
+  height: ${(props) => (props.$size ? '48px' : '32px')};
+
+  @media (max-width: 768px) {
+    width: ${(props) => (props.$size ? '48px' : '24px')};
+    height: ${(props) => (props.$size ? '48px' : '24px')};
+  }
+`
+
+export const FormContent = styled.form`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-top: 5rem;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+  }
+`
+
+export const ItemContent = styled.div`
+  width: 40%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+  margin-bottom: 8px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 10px 0 10px 0;
+    display: flex;
+    justify-content: center;
+  }
+`
+
+export const Label = styled.label`
+  width: 100%;
+  font-size: 16px;
+  color: #fbfbfb;
+  font-weight: 600;
+  line-weight: 20px;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`
+
+export const Input = styled.input<{ $size?: boolean }>`
+  width: 100%;
+  height: 28px;
+  margin-top: 20px;
+  padding: 4px;
+  background-color: #181c3c;
+  border: 1px solid #5b5fc0;
+  border-radius: 10px;
+  text-align: start;
+  font-size: 16px;
+  line-height: 18px;
+  font-weight: 300;
+  color: #8a8a8a;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`
+
+export const Select = styled.select`
+  width: 100%;
+  height: 40px;
+  margin-top: 20px;
+  padding: 4px;
+  background-color: #181c3c;
+  border: 1px solid #5b5fc0;
+  border-radius: 10px;
+  text-align: start;
+  font-size: 16px;
+  line-height: 18px;
+  font-weight: 300;
+  color: #8a8a8a;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`
+
+export const Option = styled.option`
+  width: 100%;
+  height: 30px;
+  padding: 4px;
+  background-color: #181c3c;
+  border: 1px solid #5b5fc0;
+  border-radius: 10px;
+  text-align: start;
+  font-size: 16px;
+`
+
+export const ButtonContent = styled.div`
+  width: 87%;
+  display: flex;
+  justify-content: end;
+
+  @media (max-width: 768px) {
+    width: 90%;
+    margin-top: 8px;
+  }
+`
+
+export const Button = styled.button`
+  background-color: #1ce3c7;
+  border: none;
+  border-radius: 8px;
+  padding: 8px 16px 8px 16px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #004037;
+  transition: text-shadow 0.3s ease-in-out;
 
   @media (max-width: 768px) {
     width: 100%;
