80fc058b9245984375e7f865585545e424874379
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-02-26 11:23:48 -0300

    :sparkles: feat(CreatedAccount): Adicionando conteudo e responsivo da page

---
+++ src/pages/CreatedAccount/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+  Content,
+  ContentText,
+  ContentImage,
+  LogoImage,
+  TextArea,
+  FormContent,
+  Input,
+  Text,
+  Button,
+  Label,
+  LoginLink,
+} from './style'
+import logo from '../../assets/logo.svg'
+
+const CreatedAccount = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = async () => {
+    navigate('/')
+  }
+
+  return (
+    <>
+      <Content>
+        <ContentImage>
+          <LogoImage src={logo} />
+        </ContentImage>
+        <ContentText>
+          <TextArea>
+            <Text>Cadastrar-se</Text>
+            <FormContent onSubmit={handleSubmit}>
+              <Label>Nome</Label>
+              <Input type="name" placeholder="Escreva Aqui" />
+              <Label>E-mail*</Label>
+              <Input type="email" placeholder="mail.example@gmail.com" />
+              <Label>Senha*</Label>
+              <Input type="password" placeholder="********" />
+              <Label>Confirme sua Senha*</Label>
+              <Input type="password" placeholder="********" />
+              <LoginLink to="/login">Já tenho uma conta</LoginLink>
+            </FormContent>
+            <Button type="submit">Salvar alterações</Button>
+          </TextArea>
+        </ContentText>
+      </Content>
+    </>
+  )
+}
+
+export default CreatedAccount
